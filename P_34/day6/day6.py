e1bb0d4cbc47a05e093076b4dffbdeacfe0751ec
Magesh2510 <magesh@dataterrain.com>
2024-08-14 21:18:23 +0530

    add

--- P_34/day6/day6.py
+++ P_34/day6/day6.py
@@ -57,7 +57,6 @@ print(b)
 
 print(dir(list))
 
-# , , 'copy', 'count', '', , , '', '', ''
 
 # 'append'
 # 'insert', 
@@ -68,7 +67,7 @@ print(dir(list))
 # remove - remove that element from list check after session
 # extend
 # count
-# reverse - it will be iterator
+# reverse 
 # sort
 
 # len() - can be used for many iterable datatype
@@ -179,3 +178,149 @@ print(a) # ['I', 'live','1',2,3,4 ]
 a = ['I', 'live', '1',2, '1' , 1 , 2 , 2, '1111' , '1' , 'in', 'India','1',1 ]
 print(a.count(1)) # 2
 print(a.count('1')) # 4
+
+
+a = ['I', 'live' , 'in', 'India','1',1 ]
+print(a.index(1)) # 5 
+
+# reverse a string
+a = 'I live in India'
+print(a[::-1]) # 'aidnI ni evil I'
+
+# reverse a list
+a = ['I', 'live' , 'in', 'India','1',1 ]
+print(a[::-1]) # [1, '1','India','in','live' ,'I']
+
+
+# reverse
+
+# syntax:
+# list_data.reverse()
+
+# 1. source will get changed. source i.e list_data
+
+a = ['I', 'live' , 'in', 'India','1',1 ]
+a.reverse()
+print(a)
+
+# sort
+
+# syntax:
+# list_data.sort()
+
+# 1. source will get changed. source i.e list_data
+# 2. in list all the elements should be either int+float | str
+
+a = [11,11.1,23,23432,435,12,634]
+a.sort()
+print(a)
+
+a = [11,23,'23432',435,12,634]
+a.sort() # Error
+print(a)
+
+
+a = ['a','ac','aaa','ab','aa','AA','AB']
+a.sort() # Lexicographic order || ascii value
+print(a) # ['AA','AB','a','aa','aaa','ab','ac']
+
+print(ord('a')) # 65
+print(ord('z')) # 122
+print(ord('A')) # 97
+print(ord('Z')) # 90
+print(ord('-'))
+print(chr(65)) # A
+print(chr(97)) # a
+
+# sorted()
+
+# 1. this will not update the source.
+# 2. this will give us the new output
+
+a = ['a','ac','aaa','ab','aa','AA','AB']
+b = sorted(a)
+
+print(a)
+print(b)
+
+a = [11,23,23432,435,12,634,11.1]
+a.sort()
+print(a)
+print(a[::-1])
+
+
+# it is used to avoid call by reference
+
+a = 10
+b = 15
+a = a + 5
+print(a)
+print(b)
+
+# call by value
+
+a = 10
+b = a
+a = a + 5
+print(a) # 15
+print(b) # 10
+
+# call by value
+# int
+# float
+# str
+
+# copy()
+# we will use copy to avoid call by reference to list
+
+# call by reference - same memory will be allocated
+# list
+
+a = [1,2,3,4,5]
+b = a
+a.append(6)
+b.append(16)
+print(a) # [1,2,3,4,5,6,16]
+print(b) # [1,2,3,4,5,6,16]
+
+a = [1,2,3,4,5]
+b = a.copy()
+a.append(6)
+b.append(16)
+print(a) # [1,2,3,4,5,6]
+print(b) # [1,2,3,4,5,16]
+
+print('one'+'two') # 'onetwo'
+print([1,2,3] + [4,5,6]) # [1,2,3,4,5,6]
+
+print('magesh' * 3) # that string will be dupilcated to n no of times
+# mageshmageshmagesh
+
+print(['magesh'] * 3) # that string will be dupilcated to n no of times
+# ['magesh','magesh','magesh']
+
+# Q1. a = [1,'123','magesh',345,'phone','11',234.234,'zer','Earth','Moon','Mars'] the output should be
+# 6
+# 128
+# 349
+# 16
+# 239.234
+
+# Q2. a = 'i live in India'
+# count of all the elements in a
+# ex: i is repeated for 5 times
+# ex: l is repeated for 15 times
+
+# Q3. 
+# *
+# **
+# ***
+# ****
+# *****
+
+# Q4.
+#     *
+#    **
+#   ***
+#  ****
+# *****
--- P_34/day6/tempCodeRunnerFile.py
+++ P_34/day6/tempCodeRunnerFile.py
@@ -1,3 +1,5 @@
-a = ['I', 'live', '1',2, '1' , 1 , 2 , 2, '1111' , '1' , 'in', 'India','1',1 ]
-print(a.count(1)) # 2
-print(a.count('1')) # 4
+print('magesh' * 3) # that string will be dupilcated to n no of times
+# mageshmageshmagesh
+
+print(['magesh'] * 3) # that string will be dupilcated to n no of times
+# ['magesh','magesh','magesh']
