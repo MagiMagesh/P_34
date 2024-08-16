55707fb208c950d5409ff0bf5a0d71db106d418d
Magesh2510 <magesh@dataterrain.com>
2024-08-16 20:01:37 +0530

    add

--- P_34/day7/day7.py
+++ P_34/day7/day7.py
@@ -109,3 +109,9 @@ i = 0
 while i < len(a):
     print(a[i])
     i = i + 1
+
+a = [1,1,2,3,4,34,5,543,545,6]
+i = 0
+while i < len(a):
+    print(a[i])
+    i = i + 1
--- P_34/day7/tempCodeRunnerFile.py
+++ P_34/day7/tempCodeRunnerFile.py
@@ -1,5 +1,4 @@
-a = 'i live in India'
-
+a = [1,1,2,3,4,34,5,543,545,6]
 i = 0
 while i < len(a):
     print(a[i])
