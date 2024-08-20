60a47aee4afc642689c0f3fb85acb477eda6960e
Magesh2510 <magesh@dataterrain.com>
2024-08-20 21:21:37 +0530

    add

--- P_34/day8/day8.py
+++ P_34/day8/day8.py
@@ -216,18 +216,19 @@ print(d.get('key32')) # None
 # 'clear', 'copy' - similar to list
 
 n = int(input()) # 7
-numbers = input().split() # '1 15 11 121 131 159 98'.split(' ')
+numbers = input().split() # '1 15 11 121 131 159'.split(' ')
 print(numbers)
 
-b = []
-output = None
-for i in range(len(numbers)):
-    if numbers[i]  in b:
-        output = i
-    b.append(numbers[i])
+if n == len(numbers):
+    b = []
+    output = None
+    for i in range(len(numbers)):
+        if numbers[i]  in b:
+            output = i
+        b.append(numbers[i])
 
-if output:
-    print(output)
-    print(b)
-else:
-    print(-1)
+    if output:
+        print(output)
+        print(b)
+    else:
+        print(-1)
