e8be3db22a40be153cf88be73564a1752eac337f
Magesh2510 <magesh@dataterrain.com>
2024-08-21 19:55:59 +0530

    add

--- P_34/day9/day9.py
+++ P_34/day9/day9.py
@@ -18,3 +18,56 @@ for i in d:
     print('this is key: ',i,'| this is value: ', d[i])
 
 shop = {"flowers":['Lily','Lotus','Jasmine'],"cricket":"sachin","Book":['harry','davin']}
+for i in shop:
+    if i == 'cricket':
+        print(shop[i])
+
+print(10 * '-')
+
+for i in shop:
+    print(shop['cricket'])
+
+# to print the values which are string data type:
+shop = {"flowers":['Lily','Lotus','Jasmine'],"cricket":"sachin","place":"India","solar system":"Earth","Book":['harry','davin']}
+
+for i in shop:
+    if type(shop[i]) == str:
+        print(shop[i])
+
+
+shop = {"flowers":['Magesh','Lily','Lotus','Jasmine'],"cricket":"Magesh","place":"India","solar system":"Earth","Book":['harry','davin']}
+print('Magesh' in shop) # False
+print('cricket' in shop) # True
+
+# FORMATTING:
+# 1. to create a single string
+
+i = 'Good'
+a = 'Sa'
+
+i = "Bad"
+print('this is key: ',i,'| this is value: ', a)
+a = 'Super'
+b = f"this is key: {i} | this is value: {a}"
+print(b)
+c = 'Magesh'
+print(b + c)
+
+a = input("For Name: ")
+b = input("For sex: ")
+c = 'My name is ' + a + ' My sex is ' + b
+d = f"My name is {a} My sex is {b}"
+d = f"My name is {a} My age is {2*2.5*8-2/3}"
+e = f"My name is {a} My age is {2*2.5*8-2/3 :.3f}"
+ef = f"My name is {a} My age is {10/2 :.3f}" # important
+print(c)
+print(d)
+print(e)
+print(ef)
+
+print(3.000)
+print('3.000')
+
+e = "My name is {} My age is {}".format(24,'Magesh') # positional values
+print(e)
+
--- P_34/day9/tempCodeRunnerFile.py
+++ P_34/day9/tempCodeRunnerFile.py
@@ -1,3 +1,3 @@
-d = {'key':'value','key2':'value2','key3':'value3',1:['value',2.1,'value2']}
-for i in d:
-    print('this is key: ',i,'| this is value: ', d[i])
+
+e = "My name is {} My age is {}".format(24,'Magesh') # positional values
+print(e)
