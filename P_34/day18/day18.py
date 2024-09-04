d752c9c97f76a16a48d0590b6b482fafb89272dc
Magesh2510 <magesh@dataterrain.com>
2024-09-04 19:42:52 +0530

    add

--- P_34/day18/day18.py
+++ P_34/day18/day18.py
@@ -7,14 +7,19 @@ root.geometry("400x450")
 label1 = tkinter.Label(root,text='HH:MM:SS')
 label1.pack()
 
-def start(seconds):
+def start(seconds,stoped=False):
     old_data = label1.cget('text')
     new_data  = old_data.split(':') # ['HH','MM','SS']
     new_data[-1] = str(seconds) # ['HH','MM','23']
     label1.config(text=':'.join(new_data))
     seconds = seconds + 1
 
-    root.after(1000,start,seconds) # recursive
+    if stoped:
+        return
+
+
+
+    root.after(1000,start,seconds,stoped=False) # recursive
 
 tkinter.Button(root,text=f"Start",command=lambda seconds = 0 : start(seconds)).pack()
 
@@ -32,6 +37,19 @@ def reset():
 
 button4 = tkinter.Button(root,text="Reset",command=reset)
 button4.pack()
+
+def start1(seconds):
+    print(seconds)
+    seconds = seconds + 1
+
+    if seconds == 10:
+        return False
+
+    started = root.after(1000,start1,seconds)
+
+    
+    # root.after_cancel(started)
+start1(0)
 
 
 
