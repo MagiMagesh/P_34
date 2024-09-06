c9e542029c3efc57a94db5acb01c7030cd674d5d
Magesh2510 <magesh@dataterrain.com>
2024-09-06 19:50:59 +0530

    add

---
+++ P_34/day20/dbm.py
@@ -0,0 +1,11 @@
+# 1. DBMS - school 
+# 2. tables - student,teacher,principal,admin
+# 3. column for tables
+            # student   - sno name age sex class fees rank english_mark python_mark math_mark class_teacher
+            # teacher   - sno name age sex salary class_teacher_class
+            # principal - sno name age sex salary 
+            # admin     - sno user_name password 
+# 4. only the data present in admin can add,update,delete. others can only view the tables data
+# 5. navigation to 1. student | 2. teacher | 3. principal
+# 6. if admin pick any one option in step 5  
+        #  we need to perform any CRUD operations dynamically
